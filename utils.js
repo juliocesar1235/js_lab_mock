e00039bd5a9ee7e913f15542d693221430bb3f49
Julio Gutierrez <a01282575@itesm.mx>
2020-09-11 01:56:15 -0500

    Lab finished

--- utils.js
+++ utils.js
@@ -1,24 +1,53 @@
-function get_element_li (name, price) {
-  return `<li class="added-item">name: ${name} price: ${price}  <button class="remove-item">remove</button></li>`
-}
-
-let add_item_to_list_with_template = (template_function) => {
-  return (event) => {
-    /*
-      add the item to the list
-      add event listener to the button inside the element just added with the remove_item function
-      add the value to the total
-    */
+var total_cost = 0;
+let item_name = document.querySelector("#item-name");
+let item_value = document.querySelector("#item-value");
+let item_total = document.querySelector("#total-value");
+let item_button = document.querySelector("#add-item");
+let total = document.querySelector("#total-value")
+total.textContent = 0;
+item_button.addEventListener("click", (_) => {
+  if(item_name.value != "" && item_value.value != ""){
+    item_name.classList.remove("warning")
+    item_value.classList.remove("warning")
+    add_item_to_ul(item_name.value, item_value.value, total);
+  }else{
+    item_name.classList.add("warning")
+    item_value.classList.add("warning")
   }
+})
+
+function get_element_li (name, price, total_value) {
+  let li = document.createElement('li');
+  let it_name = document.createElement('span');
+  let value = document.createElement('span');
+  let button = document.createElement('button')
+
+  button.classList.add("remove")
+  button.innerText = "remove";
+  it_name.textContent = name;
+  value.textContent = price;
+
+  button.addEventListener("click", (_) => {
+    remove_item(li, total_value)
+  });
+
+  li.appendChild(it_name)
+  li.appendChild(value)
+  li.appendChild(button);
+  return li
 }
-/*
- for removing elements could be this way
-  let element_to_delete = document.querySelector("selector").lastElementChild;
-  element_to_delete.parentNode.removeChild(element_to_delete);
-  or we could use ChildNode.remove()
-  https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
-*/
 
-let remove_item  = (node_to_remove) => {
+function add_item_to_ul(item_name, item_price, total_value){
+  total_cost += parseInt(item_price,10)
+  let li = get_element_li(item_name,item_price, total_value)
+  let ul = document.getElementById("list")
+  ul.appendChild(li)
+  total_value.textContent = total_cost
+}
 
+let remove_item  = (node_to_remove, total_value) => {
+  let ul = document.getElementById("list")
+  total_cost -= parseInt(node_to_remove.children[1].innerText)
+  total_value.textContent = total_cost
+  ul.removeChild(node_to_remove)
 }
